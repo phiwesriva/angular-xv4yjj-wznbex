97b7231d8d92d9f2613ca10ea2562bbb90f5b168
phiwesriva <phiwes@icloud.com>
2020-09-02 13:02:39 +0200

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { products } from '../products';
+
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+images = ['https://raw.githubusercontent.com/phiwesriva/angular-mn8emkeswatinipro-e6w3hm/master/src/assets/Areal.jpg', 'https://raw.githubusercontent.com/phiwesriva/angular-mn8emkeswatinipro-e6w3hm/master/src/assets/Aremax.jpg', 'https://raw.githubusercontent.com/phiwesriva/angular-mn8emkeswatinipro-e6w3hm/master/src/assets/Atech.jpg'];
+//.map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+ constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 1500;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+    config.wrap = true;
+  }
+  products = products;
+}
+}
+/*
   products = products;
 
   share() {
